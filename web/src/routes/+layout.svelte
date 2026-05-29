15e48609b2245c890040ff80275d24d2400dd37a
Selim Bucher <selim.bucher@slimmer.ch>
2026-05-30 01:32:44 +0200

    wip homepage

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -6,4 +6,10 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<nav class="flex w-full gap-4 justify-center p-4">
+	<a href=/leaderboard class="text-l hover:text-primary">Leaderboard</a>
+	<a href=/matches class="text-l hover:text-primary">Matches</a>
+	<a href=/stats class="text-l hover:text-primary">Stats</a>
+	<a href=/achievements class="text-l hover:text-primary">Achievements</a>
+</nav>
 {@render children()}
