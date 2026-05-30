ed5712ca34eb6d0e146c72d1999a33c8380085b6
Selim Bucher <selim.bucher@slimmer.ch>
2026-05-30 23:07:03 +0200

    wip

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
-
-	"github.com/selimbucher/civ6.ch/internal/api"
-	"github.com/selimbucher/civ6.ch/internal/db"
 )
 
 func main() {
-	ctx := context.Background()
-
-	pool, err := db.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer pool.Close()
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /api/players", api.HandlePlayers(pool))
 
 	log.Println("listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
